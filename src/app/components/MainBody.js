37fd65edc52c121c00b0d54e65ed28154f4e9b7c
prokoposgiannis <giannisprokopos@gmail.com>
2024-06-25 18:48:57 +0300

    test

--- src/app/components/MainBody.js
+++ src/app/components/MainBody.js
@@ -32,7 +32,7 @@ export default function MainBody() {
     {
       title: "Smarter by Words",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "This project features a minimal React website that presents a different, rare, or interesting Greek word with its definition each day. The word is then uploaded as a story on an Instagram account, which currently has over 170 active followers",
       image: "/smarter-by-words-image.jpg",
     },
     {
